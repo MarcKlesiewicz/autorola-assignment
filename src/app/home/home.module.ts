94f9a2be967a9aab7bedac10dc9669aebce1714a
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2023-08-20 13:29:08 +0200

    added material package

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuickSearchFilterModule } from '../quick-search-filter/quick-search-filter.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    QuickSearchFilterModule,
+  ]
+})
+export class HomeModule { }
