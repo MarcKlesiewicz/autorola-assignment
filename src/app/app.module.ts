0b222eb2a8d832f812d2309e87b920d0ec048a4a
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2023-08-20 14:39:54 +0200

    timer started (2h 20m) left

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { QuickSearchFilterComponent } from './quick-search-filter/quick-search-filter.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule }  from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
