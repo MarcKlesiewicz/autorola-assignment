b2864f75bab37d918817f5083e42c9b43ae37255
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2023-08-20 13:37:44 +0200

    reading articels and filters from json files

--- src/app/quick-search-filter/quick-search-filter.component.ts
+++ src/app/quick-search-filter/quick-search-filter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import FilterData from '../../assets/data/filters.json';
+import AuctionData from '../../assets/data/auction.json';
 
 @Component({
   selector: 'quick-search-filter',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./quick-search-filter.component.css']
 })
 export class QuickSearchFilterComponent {
+
+  makesList = FilterData.filters[0].data.map(make => make.value).slice(1);
+  fuelTypeList = FilterData.filters[1].data.map(fuel => fuel.value).slice(1);
+  countryList = FilterData.filters[2].data.map(country => country.value).slice(1);
+
+  allArticels = AuctionData.articles;
+
+  articels: any[] = [];
+
 
 }
