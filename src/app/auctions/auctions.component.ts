db2678e248f51aaf4854c2bc2799bf3ab3fab0be
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2023-08-20 15:51:25 +0200

    when navigating to auction page URL params is sat

--- src/app/auctions/auctions.component.ts
+++ src/app/auctions/auctions.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
 
 @Component({
   selector: 'app-auctions',
   templateUrl: './auctions.component.html',
   styleUrls: ['./auctions.component.css']
 })
-export class AuctionsComponent {
+export class AuctionsComponent implements OnInit {
+  constructor(private route: ActivatedRoute){}
 
+  ngOnInit(): void {
+    var makes: string[] = new (this.route.snapshot.params['makes']);
+    var fuelTypes: string[] = new (this.route.snapshot.params['fuelTypes']);
+    var countries: string[] = new (this.route.snapshot.params['countries']);
+   
+  }
 }
